e656af4317958bd378ab1506d70bfab740cdc290
Nikola Vukmirović <nikolavu@comtrade.com>
2025-05-08 12:36:37 +0200

    Prvi komit JMBG G

---
+++ JMBG_Generator.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import random
+
+def random_jmbg():
+    dan = random.randint(1, 31)
+    mesec = random.randint(1, 12)
+    godina = random.randint(1910, 2024)
+    
+
+
+
+
+prozor = Tk()
+prozor.title("Random JMBG generator")
+prozor.geometry("800x400")
+
+Label(prozor, text = "Serbian person generator", font='italic').pack(pady='1')
+Label(prozor, text = "Name").pack(pady='10', anchor='w')
+Label(prozor, text = "Lastname").pack(pady='10', anchor='w')
+Label(prozor, text="JMBG").pack(pady = '10', anchor = 'w')
+
+
+
+
+
+
+
+
+
+prozor.mainloop()
